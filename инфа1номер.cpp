9f9c3dc4d569a70b53220e75e4400d4d444fc3b6
Lena-Krupenko228 <lcrupenko@yandex.ru>
2025-09-26 09:46:02 +0300

    Add files via upload

---
+++ инфа1номер.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+	int i = 0;
+	int j = 1;
+	cout << "ВВедите номер машины";
+	cin >> i;
+
+	if (i < 0) {
+		cout << "Введите неотрицательное число" << endl;
+	}
+	else {
+		long long factorial = 1;
+	
+		while (j <= i) {
+			factorial = factorial * j;
+			j++;
+		}
+		cout << "производительность машин" << factorial << endl;
+	}
+
+	return 0;
+}
+
